618ca65c11acf04cc70737a70523608841828680
YungNewton <isaacnewtonahanmisi@gmail.com>
2024-07-04 11:46:54 +0100

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNewCampaignForm() {
+    document.getElementById('mainForm').classList.add('hidden');
+    document.getElementById('newCampaignForm').classList.remove('hidden');
+}
+
+function showExistingCampaignForm() {
+    document.getElementById('mainForm').classList.add('hidden');
+    document.getElementById('existingCampaignForm').classList.remove('hidden');
+}
